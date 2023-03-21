4f736aedb3623d5b9578f424fe2270c7ff7342af
Wang, Mingjie1 <mingjie1.wang@intel.com>
2023-03-21 18:25:54 -0500

    Fixed Windows issue.

--- conda-recipe/repack.bat
+++ conda-recipe/repack.bat
@@ -10,6 +10,6 @@ popd
 pushd %SRC_DIR%\compiler
 %PYTHON% -c "import dpcpp_llvm_spirv as p; print(p.get_llvm_spirv_path())" > Output
 set /p DIRSTR= < Output
-copy bin-llvm\llvm-spirv %DIRSTR%
+copy Library\bin-llvm\llvm-spirv.exe %DIRSTR%
 del Output
 popd
